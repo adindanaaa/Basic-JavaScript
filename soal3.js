e92b7dc95247d9dba06de1a3ec2ddeed05ddcd1c
adindanaaa <adinda_kharisma_29rpl@student.smktelkom-mlg.sch.id>
2022-01-19 17:14:16 +0700

    Assignment

---
+++ soal3.js
@@ -0,0 +1,14 @@
+let nama = "Rhodey", bb = 90, tb = 1.7
+let hitung = bb / (tinggi*tinggi);
+
+console.log(`BMI = ${hitung}`);
+
+if (hitung < 18.5) {
+    console.log("Kurang Berat Badan");
+} else if(hitung >= 18.5 && hitung <= 24.9){
+    console.log("Normal");
+} else if(hitung >= 25 && hitung <= 29.9){
+    console.log("Kelebihan Berat Badan");
+} else if(hitung > 30){
+    console.log("Kegemukan");
+}
